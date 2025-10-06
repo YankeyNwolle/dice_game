a7c0f2f47c7d37907cf626677a6b0b3b8a9e401d
Yankey Ange <nwolle14@gmail.com>
2025-10-06 19:31:56 +0000

    second changes

--- index.js
+++ index.js
@@ -2,34 +2,18 @@
 
 const images = ["images/dice1.png", "images/dice2.png", "images/dice3.png","images/dice4.png","images/dice5.png","images/dice6.png"];
 
+// génération des indices aléatoires
+const indicealeatoire1 = Math.floor(Math.random() * images.length);
+const indicealeatoire2 = Math.floor(Math.random() * images.length);
 
-// création des images
-const images1 = document.querySelector('.img1');
+// affichage de l'images 1 alétoirement
+const affiche_image_1 = images[indicealeatoire1];
 
-const images2 = document.querySelector('.img2');
+document.getElementById("img1").src = affiche_image_1;
 
+// image 2 aléatoire
 
-// obtention de l'indice aléatoire
+const affiche_image_2 = images[indicealeatoire2];
 
-const indiceAleatoire = Math.floor(Math.random() * images.length);
-
-// 3. Sélectionnez l'image
-const imageAffiche1 = images1[indiceAleatoire];
-const imageAffiche2 = images2[indiceAleatoire];
-
-// 4. Créez et affichez l'élément image
-const nouvelleImage1 = document.createElement("img1");
-nouvelleImage1.src = imageAffiche1;
-console.log(nouvelleImage1);
-
-const divImage1 = document.getElementById(".img1");
-divImage1.appendChild(nouvelleImage1);
-
-// nouvelle images 2
-const nouvelleImage2 = document.createElement("img2");
-nouvelleImage2.src = imageAffiche2;
-
-const divImage2 = document.getElementById(".img2");
-divImage2.appendChild(nouvelleImage2);
-
+document.getElementById("img2").src = affiche_image_2;
 
